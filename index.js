5bdf8791bb5f173b7a5e4073a1a329140feddae6
JuniorN1 <junior_e.a.o@hotmail.com>
2020-11-15 22:08:00 -0300

    pegando imagems do intagram e salvando

--- index.js
+++ index.js
@@ -1,22 +1,40 @@
- const puppeteer = require('puppeteer');
- (async () => {
-            const browser = await puppeteer.launch({handler:false});
-            const page = await browser.newPage();
-            await page.goto('https://www.instagram.com/ibgeoficial/');
-            await page.evaluate(()=>{
-                
-                const listDados = document.querySelector('article img');
-                const arrayList = [...listDados];
-                
+const puppeteer = require('puppeteer');
+const fs = require('fs');
 
-               const list = arrayList.map(img=>{
-                    return {
-                        a:img
-                    }
-                })
-            console.log(list);
-            }
-            );
+
+async function screen(){
+    const teste = await
+    (async () => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto('https://www.instagram.com/acnurbrasil/');
+   
+    const listImg = await page.evaluate(()=>{
+        const seachImg = document.querySelectorAll('main article img');
+        const arrayResults = [...seachImg];
+
+        const listImg= arrayResults.map(img=>{
+            return img.src
+
+        })
         
-            await browser.close();
-        })();
+     return listImg
+    })
+
+    fs.writeFile('instagram.json',JSON.stringify(listImg,null,2),err=>{
+        if(err) throw new Error('something sent wrong');
+        console.log('well done')
+    })
+
+    await browser.close();
+    })();
+    return "pontro";
+}
+console.clear();
+console.log("loading")
+screen().then(()=>{
+    console.clear();
+    console.log('pronto');
+}
+)
+
